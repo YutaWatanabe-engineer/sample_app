45bbc5bf32fd78548df314b498661ab77fdc267e
YutaWatanabe-engineer <watanabemediapro@gmail.com>
2020-10-21 22:04:42 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   # GET /users/:id
   
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+
+  end
+
   def show
     @user = User.find(params[:id])
     
@@ -26,6 +35,30 @@ class UsersController < ApplicationController
     end
   end
 
+  #GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 更新に成功した場合を扱う。
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
+  end
+
+
   #private
 
     def user_params
@@ -33,7 +66,25 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
   def destroy
-    log_out
-    redirect_to root_url
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
+  def logged_in_user
+    unless logged_in?
+      store_location
+
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_action :logged_in_user, only: [:edit, :update]
+
   before_save{self.email = self.email.downcase}#保存する前に大文字を小文字にしよう
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,13 +10,19 @@ class User < ApplicationRecord
 
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
   
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 
 end
